b7827532e6a3553eccc5d3da8ca5bc590186c4b3
aaronmee <114234515+aaronmee@users.noreply.github.com>
2024-09-16 14:26:39 +0200

    Add title text

---
+++ Assets/df.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class df : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI titleText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleText.text = "hallo";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
