f3c99ac4e9f1c4d4eb823206093a728345b600fd
Nalim20000 <milan.richter.sus@gmail.com>
2024-09-17 00:47:34 +0200

    Ha probiert ä funktion zum seperierä z ergänzä
    Ha ä funktion gschribä woni nid sicher bi öb si geit, ä vrglich mit phyton isch gangä.
    Bitte Drüberluägä

--- Assets/Scripts/MainDeckScript.cs
+++ Assets/Scripts/MainDeckScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MainDeckScript : MonoBehaviour
 {
     [SerializeField] public List<GameObject> cardPile;
+    public List<GameObject> playerPile;
+    public List<GameObject> computerPile;
     //Creates a list wich is named cardPile with every cardobjekt 
     // Start is called before the first frame update
     void Shuffle<T>(List<T> list)
@@ -19,10 +21,25 @@ public class MainDeckScript : MonoBehaviour
         }
     }
     //Used chatgpt to create this function
+    //The function randomises a given list out of gameobjekts
+    void Seperate<T>(List<T> list)
+    {
+        for (int i = (list.Count /2)-1; i > 0; i--)
+        {
+            playerPile.Add(cardPile[i]);
+            computerPile.Add(cardPile[list.Count/2+ i]);
+        }
+
+    }
+    //The function seperates the schouffled list in to tow card decks
+
+
     void Start()
     {
         Shuffle(cardPile);
         //when the programm starts the list gets schouffled.
+        Seperate(cardPile);
+        //When the programm starts the card pile gets seperatet.
     }
 
 
@@ -32,3 +49,4 @@ public class MainDeckScript : MonoBehaviour
 
     }
 }
+
