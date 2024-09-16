f8fb8db8303e9e40b206eb2e7282793b60f6d00e
Nalim20000 <milan.richter.sus@gmail.com>
2024-09-16 16:20:48 +0200

    Nöis script fürs main deck
    Tag maindeck drzuä
    Script Maindeck ersteut
    Am gameobjekt täg drzuägfüägt
    Im Script ä listä gmacht u aui cardobjects drzuä ta

---
+++ Assets/Scripts/MainDeckScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MainDeckScript : MonoBehaviour
+{
+    [SerializeField] public List<GameObject> cardPile;
+    //Inputfield for unity
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
