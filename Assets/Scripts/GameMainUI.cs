b23d00e3f5da4c04d6f0e877803f9b66ca938c2d
aaronmee <114234515+aaronmee@users.noreply.github.com>
2024-09-19 09:46:23 +0200

    Coded Game Scene back and exit buttons

--- Assets/Scripts/GameMainUI.cs
+++ Assets/Scripts/GameMainUI.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameMainUI : MonoBehaviour
 {
@@ -10,12 +12,21 @@ public class GameMainUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI playerCardCounterText;
     [SerializeField] private TextMeshProUGUI computerCardCounterText;
 
+    [SerializeField] private Button exitButton;
+    [SerializeField] private Button backButton;
+
 
 
     private void Awake()
     {
         playerCardCounterText.text = "16 Karten";
         computerCardCounterText.text = "16 Karten";
+
+        exitButton.onClick.AddListener(() => Application.Quit());
+        backButton.onClick.AddListener(() => SceneManager.LoadScene("MainMenuScene"));
+
+        // GameManager.Instance.OnPlayerVictory += ...
+        // GameManager.Instance.OnPlayerDefeat += ...
     }
 
     void Update()
@@ -23,4 +34,17 @@ public class GameMainUI : MonoBehaviour
         //playerCardCounterText.text = GameManager.Instance.playerPile.Count + " Karten";
         //computerCardCounterText.text = GameManager.Instance.computerPile.Count + " Karten";
     }
+
+
+    private void OnPlayerVictory()
+    {
+        backButton.gameObject.SetActive(false);
+    }
+
+    private void OnPlayerDefeat()
+    {
+        backButton.gameObject.SetActive(false);
+    }
+
+
 }
