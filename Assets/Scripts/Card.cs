cec209ed4b2a85bec69efda6d776b49548fa7285
MOUMOU333 <167429098+MOUMOU333@users.noreply.github.com>
2024-09-19 18:04:13 +0200

    dabkomment
    made kommentos

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -43,48 +43,93 @@ public class Card : MonoBehaviour
     }
     public void AnimatePlayerToPile(Vector3 addVector)
     {
+        // Function that takes a vector as an argument and has no output.
+        // It animates a card from the playerDeck to the players cardPile
+        // and flips it over, so we see its frontside instead of the back.
+        // It needs the vector to move it up the z axis and down the y axis
+        // so that the Card isnt covered up by the card that are already on
+        // the pile
         StartCoroutine(AnimatePlayerMove(pPilePosition, addVector));
         StartCoroutine(AnimateFlip());
     }
     public void AnimatePlayerToDeck()
     {
+        // Function that takes no argument and has no output.
+        // It animates a card from the playerPile to the players Deck
+        // and flips it over, so we see its put back inside the deck with
+        // it's backside up instead of the front.
+
         StartCoroutine(AnimatePlayerMove(pDeckPosition, zeroVector));
         StartCoroutine(AnimateFlip());
     }
     public void AnimatePlayerToPileDraw(Vector3 addVector)
     {
+        // Function that takes a vector as an argument and has no output.
+        // It animates a card from the playerDeck to the players cardPile
+        // in case of a draw. We dont flip the card, because it isn't used
+        // for comparison. It needs the vector to move it up the z axis and
+        // down the y axis so that the Card isnt covered up by the card that
+        // are already on the pile
         cardData.isTribut = true;
         StartCoroutine(AnimatePlayerMove(pPilePosition, addVector));
     }
     public void AnimatePlayerToDeckDraw()
     {
+        // Function that takes no argument and has no output.
+        // It animates a card from the playerDeck to the players cardPile
+        // in case of a draw. We dont flip the card, because it wasn't used
+        // for comparison and therefore already is backside up.
         cardData.isTribut = false;
         StartCoroutine(AnimatePlayerMove(pDeckPosition, zeroVector));
     }
     public void AnimateComputerToPile(Vector3 addVector)
     {
+        // Function that takes a vector as an argument and has no output.
+        // It animates a card from the computerDeck to the computers cardPile.
+        // It needs the vector to move it up the z axis and down the y axis
+        // so that the Card isnt covered up by the card that are already on
+        // the pile
         StartCoroutine(AnimateComputerMove(cPilePosition, addVector));
         StartCoroutine(AnimateFlip());
     }
     public void AnimateComputerToDeck()
     {
+        // Function that takes no argument and has no output.
+        // It animates a card from the computerPile to the computer Deck
+        // and flips it over, so we see its put back inside the deck with
+        // it's backside up instead of the front.
         StartCoroutine(AnimateComputerMove(cDeckPosition, zeroVector));
         StartCoroutine(AnimateFlip());
     }
     public void AnimateComputerToPileDraw(Vector3 addVector)
     {
+        // Function that takes a vector as an argument and has no output.
+        // It animates a card from the computerDeck to the computers cardPile
+        // in case of a draw. We dont flip the card, because it isn't used
+        // for comparison. It needs the vector to move it up the z axis and
+        // down the y axis so that the Card isnt covered up by the card that
+        // are already on the pile.
         cardData.isTribut = true;
         StartCoroutine(AnimateComputerMove(cPilePosition, addVector));
 
     }
     public void AnimateComputerToDeckDraw()
     {
+        // Function that takes no argument and has no output.
+        // It animates a card from the computerDeck to the computers cardPile
+        // in case of a draw. We dont flip the card, because it wasn't used
+        // for comparison and therefore already is backside up.
         cardData.isTribut = false;
         StartCoroutine(AnimateComputerMove(cDeckPosition, zeroVector));
     }
 
     private IEnumerator AnimatePlayerMove(Vector3 position, Vector3 addVector)
     {
+        // This function takes a two Vectors as arguments and has the output:
+        // yield return tweenMove.WaitForCompletion(); It uses the vectores to
+        // determin the position the cards should be moved to. This function
+        // specifically animates the players cards
+
         position = position + addVector;
         if (tweenMove == null)
             tweenMove = transform
@@ -101,6 +146,10 @@ public class Card : MonoBehaviour
 
     private IEnumerator AnimateComputerMove(Vector3 position, Vector3 addVector)
     {
+        // This function takes a two Vectors as arguments and has the output:
+        // yield return tweenMove.WaitForCompletion(); It uses the vectores to
+        // determin the position the cards should be moved to. This function
+        // specifically animates the computers cards
         position = position + addVector;
         if (tweenMove == null)
             tweenMove = transform
--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -4,31 +4,35 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
+//all comments form chatgpt
 public class MainMenuUI : MonoBehaviour
 {
+    // Serialized fields for the buttons that can be set in the Unity Inspector
+    [SerializeField] private Button singlePlayerButton;  // Button for single player mode
+    [SerializeField] private Button multiPlayerButton;   // Button for multiplayer mode
+    [SerializeField] private Button tutorialButton;      // Button for tutorial mode
+    [SerializeField] private Button exitButton;          // Button to exit the game
 
-    [SerializeField] private Button singlePlayerButton;
-    [SerializeField] private Button multiPlayerButton;
-    [SerializeField] private Button tutorialButton;
-    [SerializeField] private Button exitButton;
-
-
-
-
+    // Function: Awake
+    // Purpose: Initializes the button listeners and sets the target frame rate.
+    // Arguments: None
+    // Output: None
     private void Awake()
     {
+        // Adds listener for the single player button to load the "GameScene".
         singlePlayerButton.onClick.AddListener(() => SceneManager.LoadScene("GameScene"));
 
+        // Adds listener for the multiplayer button, but the functionality is not implemented yet.
         multiPlayerButton.onClick.AddListener(() => Debug.Log("Coming soon!"));
 
+        // Adds listener for the tutorial button to load the "TutorialScene".
         tutorialButton.onClick.AddListener(() => SceneManager.LoadScene("TutorialScene"));
 
+        // Adds listener for the exit button to quit the game application.
         exitButton.onClick.AddListener(() => Application.Quit());
 
+        // Sets the target frame rate to 60 frames per second.
         Application.targetFrameRate = 60;
     }
-
-
-
-
 }
